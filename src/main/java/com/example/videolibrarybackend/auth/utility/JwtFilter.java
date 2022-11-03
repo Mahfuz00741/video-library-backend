900ba9543e9d93794de420ca4151b0092ed502da
mahfuz.rahman <mahfuz.rahman@ibcs-primax.com>
2022-11-03 13:00:44 +0600

    token generated with mail and password

--- src/main/java/com/example/videolibrarybackend/auth/utility/JwtFilter.java
+++ src/main/java/com/example/videolibrarybackend/auth/utility/JwtFilter.java
@@ -1,10 +1,10 @@
 package com.example.videolibrarybackend.auth.utility;
 
+import com.example.videolibrarybackend.auth.service.implementation.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,9 +19,9 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
-    private JwtUtility jwtUtil;
+    private JwtUtil jwtUtil;
     @Autowired
-    private UserDetailsService service;
+    private CustomUserDetailsService service;
 
 
     @Override
